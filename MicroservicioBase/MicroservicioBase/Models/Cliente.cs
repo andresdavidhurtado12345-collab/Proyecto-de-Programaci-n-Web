0436cfccef4612bf282d7f1f8c596079d9800b88
andresdavidhurtado12345-collab <andresdavidhurtado12345@gmail.com>
2025-09-18 10:13:18 -0500

    Parcial de Progranación

---
+++ MicroservicioBase/MicroservicioBase/Models/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MicroservicioBase.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MinLength(3, ErrorMessage = "El nombre debe tener al menos 3 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [EmailAddress(ErrorMessage = "El correo no es válido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El teléfono es obligatorio")]
+        [RegularExpression("^[0-9]+$", ErrorMessage = "El teléfono solo debe contener números")]
+        public string Telefono { get; set; }
+
+        public bool Estado { get; set; } = true;
+    }
+
+}
